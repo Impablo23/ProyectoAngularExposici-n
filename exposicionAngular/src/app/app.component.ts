dc80e65739f22b1649b596562f01f49ce0b88e70
Impablo23 <pabloricomachio21812@gmail.com>
2025-02-24 22:11:40 +0100

    inicio de proyecto

---
+++ exposicionAngular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'exposicionAngular';
+
+  public nombre: string = 'Pablo y José Luis';
+}
